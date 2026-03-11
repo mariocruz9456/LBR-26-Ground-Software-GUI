59696f749d66bc1b5071da94da2b46045504813e
mariocruz9456 <mario.david.cruz01@gmail.com>
2026-03-11 03:08:10 -0700

    -m "updated main.py"

--- src/ground_gui/main.py
+++ src/ground_gui/main.py
@@ -1,1 +1,24 @@
+import sys
+from PySide6 import QtWidgets
+from PySide6 import QApplication
 
+# Mock data just to test if the GUI works
+from ground_gui.data.mock_data import mockdata
+
+# Main window of GUI
+from ground_gui.ui.main_window import mainwindow
+
+def main():
+    app = QApplication(sys.argv)
+    app.setApplicationName("Ground GUI")
+
+    source = mockdata(interval = 1000)
+
+    window = mainwindow(datasource = source)
+
+    window.show()
+
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
